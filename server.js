d92b77a4116727618f9e4f2cd0a9ea5fa8f25a7e
Anastasia Boshniak <34798026+B-Anastasia@users.noreply.github.com>
2022-01-10 20:08:17 +0200

    NPM & Packages

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res)=>{
     console.log('Server request');
+    console.log('Server test');
     console.log(req.url, req.method);
 
     res.setHeader('Content-Type', 'text/html');
